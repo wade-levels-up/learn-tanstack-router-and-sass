4a762bb04db98b5553d3547dc03ef427bca96ec8
Wade Foster The Black Machine <172018441+wade-levels-up@users.noreply.github.com>
2025-10-04 17:52:05 +1000

    feat: Add routing and components for Profile, Error, DefaultProfile, Popeye, and Spinach

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,8 +1,22 @@
+import { useParams } from "react-router";
+import Popeye from "./Popeye";
+import Spinach from "./Spinach";
+import DefaultProfile from "./DefaultProfile";
+
 const Profile = () => {
+  const { name } = useParams();
+
   return (
     <div>
       <h1>Hello from the profile page!</h1>
       <p>So, how are you?</p>
+      {name === "popeye" ? (
+        <Popeye />
+      ) : name === "spinach" ? (
+        <Spinach />
+      ) : (
+        <DefaultProfile />
+      )}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.scss";
-import App from "./App.tsx";
-import Profile from "./Profile.tsx";
+import routes from "./routes.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
