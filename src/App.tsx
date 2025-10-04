49b73d39dae5236182010db65b57aa5053f4522b
Wade Foster The Black Machine <172018441+wade-levels-up@users.noreply.github.com>
2025-10-04 15:30:14 +1000

    feat: Add React Router and include Profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router";
+
 export default function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>Hello! This is the Home page</h1>
+      <p>This page uses a Sass scss file for it's styling!</p>
+      <ul>
+        <li>
+          <Link to="profile">Profile Page</Link>
+        </li>
+      </ul>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.scss";
 import App from "./App.tsx";
+import Profile from "./Profile.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
