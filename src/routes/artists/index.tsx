f111792218923110a9ffe39e2048291d67a4d096
Wade Foster The Black Machine <172018441+wade-levels-up@users.noreply.github.com>
2025-10-05 20:14:21 +1100

    feat: update artists route to use useQuery for data fetching

--- src/routes/artists/index.tsx
+++ src/routes/artists/index.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 type Artist = {
@@ -8,22 +9,31 @@ type Artist = {
 
 export const Route = createFileRoute("/artists/")({
   component: ArtistsComponent,
-  loader: async () => {
-    const artists = await fetch("http://localhost:3000/artists").then((res) =>
-      res.json()
-    );
-    return artists;
-  },
+  // loader: async () => {
+  //   const artists = await fetch("http://localhost:3000/artists").then((res) =>
+  //     res.json()
+  //   );
+  //   return artists;
+  // },
 });
 
 function ArtistsComponent() {
-  const { artists } = Route.useLoaderData();
+  const { isPending, error, data } = useQuery({
+    queryKey: ["artistsData"],
+    queryFn: () =>
+      fetch("http://localhost:3000/artists").then((res) => res.json()),
+  });
+
+  if (isPending) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const { artists } = data;
 
   return (
     <>
       <h1>Artists registered with MegaDorsal SoundScoper</h1>
       <ul>
-        {artists?.map((artist: Artist, index: number) => (
+        {artists.map((artist: Artist, index: number) => (
           <li key={index}>
             <p>Artist name: {artist.name}</p>
             <p>Artist age: {artist.age}</p>
